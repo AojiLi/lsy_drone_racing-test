b0eef4fc2a21c5f321981ebb76bad37a0e9a5a46
stateirving <miaoxiaochen0701@gmail.com>
2026-06-17 21:40:59 +0200

    level3 relax

--- lsy_drone_racing/control/ppo_level3_inference.py
+++ lsy_drone_racing/control/ppo_level3_inference.py
@@ -24,7 +24,7 @@ if TYPE_CHECKING:
     from numpy.typing import NDArray
 
 
-MODEL_NAME = "checkpoints/level3_DR_initial/level3_DR_initial_final.ckpt"
+MODEL_NAME = "checkpoints/level3_DR_initial/level3_DR_initial_step_040000000.ckpt"
 
 
 N_HISTORY = 2
